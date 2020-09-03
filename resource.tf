9ad62c7fccab900c597930853171da621ead1e6e
Cryofracture <thecryoforce@gmail.com>
2020-09-02 21:11:14 -0500

    Add environment variable to resource.tf

--- resource.tf
+++ resource.tf
@@ -13,8 +13,8 @@ variable "AMIS" {
 }
 
 provider "aws" {
-    access_key = $AWS_ACCESS_KEY
-    secret_key = $AWS_SECRET_KEY
+    access_key = "$AWS_ACCESS_KEY"
+    secret_key = "$AWS_SECRET_KEY"
     region     = var.AWS_REGION
 }
 
