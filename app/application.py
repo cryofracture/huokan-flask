8c9bee865c9f2f982fb3d10de3d84e3d4aec13ad
Cryofracture <thecryoforce@gmail.com>
2020-09-06 02:13:43 -0500

    Add some explanatory comments to application.py, remove comment from Dockerfile, adjust variable declaration in resource.tf

--- app/application.py
+++ app/application.py
@@ -8,21 +8,28 @@ import os
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-
+# Load the environment variables, this module searches for .env first, but you can directly name the file if desired.
 load_dotenv()
 app = Flask(__name__)
+# Bootstrap is fast, light, and super customizeable. Love it.
 boostrap = Bootstrap(app)
+
+# CSRF token
 app.config['SECRET_KEY'] = uuid.uuid1()
 
 
 
 @app.route('/')
 def main():
+    # Home page, enter Customer name-realm and Advertiser Name-realm.
     title = "New"
     return render_template('main.html', title=title)
 
 @app.route('/generated_id', methods = ['POST', 'GET'])
 def generated():
+    # Generate a unique ID for the customer. Copy the names from the previous page.
+    # Delete spaces from the string so names like ("Cryofracture - Area 52    ") get stripped to ("Cryofracture-Area52").
+    title           = "Confirmation"
     customer_uuid   = uuid.uuid1()
     customer_name   = request.form.get("customerName")
     advertiser_name = request.form.get("advertiserName")
@@ -30,61 +37,57 @@ def generated():
     advertiser_name = advertiser_name.replace(" ", "")
     
 
-    return render_template('generated.html', customer_name=customer_name, customer_uuid=customer_uuid, advertiser_name=advertiser_name)
+    return render_template('generated.html', customer_name=customer_name, customer_uuid=customer_uuid, advertiser_name=advertiser_name, title=title)
 
 @app.route('/creation_confirmation', methods = ['POST'])
 def confirmation():
+    # Again copy the UUID from the 'confirmation' page, and both names.
+    # Create a timestamp in postgres 'TIMESTAMP' (timestamp without timezone) format.
+    # Effectively 'escape' single quotes in the Realm Names, by doubling the single quote on insert to the database.
     title           = "New Generated ID"
     customer_uuid   = request.form.get("customerID")
     customer_name   = request.form.get("customerName")
     advertiser_name = request.form.get("advertiserName")
-    create_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    customer_name = customer_name.replace("'", "''")
+    create_date     = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    customer_name   = customer_name.replace("'", "''")
     advertiser_name = advertiser_name.replace("'", "''")
     
-    
+    # Sends the request for the 'new' customer to the database. If the application catches an error, in this case the customer name already existing, return the existing customer's ID.
     try:
-        connection = psycopg2.connect(host = os.environ['DB_HOST'],
-                                    port = os.environ['DB_PORT'],
-                                    database = os.environ['DB_NAME'],
-                                    user = os.environ['DB_USER'],
-                                    password = os.environ['DB_PASS'])
+        connection = psycopg2.connect(host          = os.environ['DB_HOST'],
+                                        port        = os.environ['DB_PORT'],
+                                        database    = os.environ['DB_NAME'],
+                                        user        = os.environ['DB_USER'],
+                                        password    = os.environ['DB_PASS'])
 
         query = f""" INSERT INTO {os.environ['DB_SCHEMA']}.{os.environ['DB_TABLE']} ({os.environ['DB_COLS']}) VALUES ('{customer_uuid}','{customer_name}','{advertiser_name}','{create_date}') """
 
         cursor = connection.cursor()
         cursor.execute(query)
         connection.commit()
-    #print(query)
 
     except psycopg2.Error as error:
-        title = "Existing Customer Found"
-        error = error.pgcode
-        connection = psycopg2.connect(host = os.environ['DB_HOST'],
+        title       = "Existing Customer Found"
+        error       = error.pgcode
+        connection  = psycopg2.connect(host = os.environ['DB_HOST'],
                                       port = os.environ['DB_PORT'],
                                       database = os.environ['DB_NAME'],
                                       user = os.environ['DB_USER'],
                                       password = os.environ['DB_PASS'])
 
-        query = f""" SELECT {os.environ['TABLE_COLS']} FROM {os.environ['DB_SCHEMA']}.{os.environ['DB_TABLE']} WHERE customer_name LIKE '{customer_name}' """
-        # print(query)
-        cursor = connection.cursor()
+        query   = f""" SELECT {os.environ['TABLE_COLS']} FROM {os.environ['DB_SCHEMA']}.{os.environ['DB_TABLE']} WHERE customer_name LIKE '{customer_name}' """
+        cursor  = connection.cursor()
         cursor.execute(query)
         connection.commit()
-        existing_info = list(cursor.fetchone())
-        customer_name = existing_info[0]
-        customer_id = existing_info[1]
-        created_date = existing_info[2]
-        customer_name = customer_name.replace("''", "'")
-        # print(existing_info[0])
-        # print(existing_info[1])
-        # print(existing_info[2])
 
-        #flash('Customer already enrolled in rewards program. Please use existing ID.')
+        existing_info   = list(cursor.fetchone())
+        customer_name   = existing_info[0]
+        customer_id     = existing_info[1]
+        created_date    = existing_info[2]
+        customer_name   = customer_name.replace("''", "'")
 
         return render_template('duplicate.html', title=title, customer_name=customer_name, customer_id=customer_id, created_date=created_date)
 
-    #print(existing_info)
     advertiser_name = advertiser_name.replace("''", "'")
     customer_name = customer_name.replace("''", "'")
 
@@ -92,4 +95,4 @@ def confirmation():
 
 
 if __name__ == '__main__':
-    app.run()#debug=os.environ['FLASK_DEBUG_STATUS'], port=os.environ['FLASK_RUN_PORT'], host=os.environ['FLASK_RUN_HOST'])
+    app.run()
--- resource.tf
+++ resource.tf
@@ -14,10 +14,12 @@ variable "AMIS" {
 
 variable "AWS_ACCESS_KEY" {
     type = string
+    default = "$AWS_ACCESS_KEY"
 }
 
 variable "AWS_SECRET_KEY" {
     type = string
+    default = "$AWS_SECRET_KEY"
 }
 
 provider "aws" {
