1b5cddbb35293a5a27548a0adf6c7350fd9c9a87
Cryofracture <thecryoforce@gmail.com>
2020-09-14 04:07:45 -0500

    Added multiple HTML pages and flask routes. Fleshing out web app with additional features.

--- app/application.py
+++ app/application.py
@@ -6,24 +6,24 @@ from dotenv import load_dotenv
 from datetime import datetime
 import os
 from werkzeug.security import generate_password_hash, check_password_hash
-
+import re
+import hashlib
+#from flask.ext.navigation import Navigation
 
 # Load the environment variables, this module searches for .env first, but you can directly name the file if desired.
 load_dotenv()
 app = Flask(__name__)
-# Bootstrap is fast, light, and super customizeable. Love it.
+# Bootstrap. Love it.
 boostrap = Bootstrap(app)
 
 # CSRF token
 app.config['SECRET_KEY'] = uuid.uuid1()
 
-
-
 @app.route('/')
 def main():
     # Home page, enter Customer name-realm and Advertiser Name-realm.
     title = "New"
-    return render_template('main.html', title=title)
+    return render_template('main.html', title=title, active=True)
 
 @app.route('/generated_id', methods = ['POST', 'GET'])
 def generated():
@@ -37,7 +37,7 @@ def generated():
     advertiser_name = advertiser_name.replace(" ", "")
     
 
-    return render_template('generated.html', customer_name=customer_name, customer_uuid=customer_uuid, advertiser_name=advertiser_name, title=title)
+    return render_template('generated.html', customer_name=customer_name, customer_uuid=customer_uuid, advertiser_name=advertiser_name, title=title, active=True)
 
 @app.route('/creation_confirmation', methods = ['POST'])
 def confirmation():
@@ -77,6 +77,7 @@ def confirmation():
 
         query   = f""" SELECT {os.environ['TABLE_COLS']} FROM {os.environ['DB_SCHEMA']}.{os.environ['DB_TABLE']} WHERE customer_name LIKE '{customer_name}' """
         cursor  = connection.cursor()
+        
         cursor.execute(query)
         connection.commit()
 
@@ -86,13 +87,170 @@ def confirmation():
         created_date    = existing_info[2]
         customer_name   = customer_name.replace("''", "'")
 
-        return render_template('duplicate.html', title=title, customer_name=customer_name, customer_id=customer_id, created_date=created_date)
+        return render_template('duplicate.html', title=title, customer_name=customer_name, customer_id=customer_id, created_date=created_date, active=True)
 
     advertiser_name = advertiser_name.replace("''", "'")
     customer_name = customer_name.replace("''", "'")
 
-    return render_template('confirmation.html', customer_name=customer_name, customer_uuid=customer_uuid, advertiser_name=advertiser_name, create_date=create_date, title=title)
+    return render_template('confirmation.html', customer_name=customer_name, customer_uuid=customer_uuid, advertiser_name=advertiser_name, create_date=create_date, title=title, active=True)
 
+
+
+@app.route('/customer/lookup', methods = ['GET'])
+def lookup():
+    # Lookup page, enter Customer name-realm to get existing UUID.
+    title = "Customer Lookup"
+    return render_template('lookup.html', title=title, active=True)
+
+@app.route('/customer/lookup/results', methods = ['GET'])
+def lookup_results():
+    # Lookup page, enter Customer name-realm to get existing UUID.
+    title = "Lookup Results"
+    return render_template('lookup_results.html', title=title, active=True)
+
+@app.route('/registration', methods = ['GET'])
+def registration():
+    # Registration page, enter Advertiser name-realm and desired password (encrypted before application sees it).
+    title = "Advertiser Registration"
+
+    return render_template('registration.html', title=title, active=True)
+
+@app.route('/registration/confirmation', methods = ['POST'])
+def registration_confirmation():
+    advertiser_name = request.form.get('name')
+    advertiser_email = request.form.get('email')
+    password1 = request.form.get('psw')
+    password2 = request.form.get('psw-repeat')
+    advertiser_name = advertiser_name.replace(" ", "")
+    if advertiser_email.contains("@") and advertiser_email.contains("."):
+        if password1 == password2:
+            pass_flag = 0
+            while True:
+                if(len(password)<10):
+                    flag = -1
+                    error = "Warning: Password too short."
+                    return redirect(url_for('registration'), title=title, active=True, error=error)
+                elif not re.search("[a-z]", password):
+                    flag = -1
+                    error = "Must contain 1 lower case."
+                    return redirect(url_for('registration'), title=title, active=True, error=error)
+                elif not re.search("[A-Z]", password):
+                    flag = -1
+                    error = "Must contain 1 upper case."
+                    return redirect(url_for('registration'), title=title, active=True, error=error)
+                elif not re.search("[0-9]", password):
+                    flag = -1
+                    error = "Must contain a number."
+                    return redirect(url_for('registration'), title=title, active=True, error=error)
+                elif not re.search("[_!@#$%^&*()_+-=[{\]};:'\",<.>\/\?]", password):
+                    flag = -1
+                    error = "Must contain a special character."
+                    return redirect(url_for('registration'), title=title, active=True, error=error)
+                elif re.search("\s", password):
+                    flag = -1
+                    error = "Cannot contain a space."
+                    return redirect(url_for('registration'), title=title, active=True, error=error)
+                else:
+                    flag = 0
+                    password            = password1.encode()
+                    salt                = os.environ['SALT_PASS'].encode()
+                    password_hash       = hashlib.pbkdf2_hmac("sha256", password, salt, 100000)
+                    advertiser_email    = hashlib.pbkdf2_hmac("sha256", advertiser_email, salt, 100000)
+
+                    try:
+                        create_date         = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                        advertiser_name     = advertiser_name.replace("'", "''")
+                        advertiser_id       = uuid.uuid1()
+
+                        connection = psycopg2.connect(host          = os.environ['DB_HOST'],
+                                                        port        = os.environ['DB_PORT'],
+                                                        database    = os.environ['REG_DB_NAME'],
+                                                        user        = os.environ['DB_USER'],
+                                                        password    = os.environ['DB_PASS'])
+
+                        query = f""" INSERT INTO {os.environ['DB_SCHEMA']}.{os.environ['REG_TABLE']} ({os.environ['REG_COLS']}) VALUES ('{advertiser_id}','{advertiser_name}','{advertiser_email}','{create_date}', '{password_hash}') """
+
+                        cursor = connection.cursor()
+                        cursor.execute(query)
+                        connection.commit()
+                        return render_template(url_for('registration_confirmation'), title=title, advertiser_id=advertiser_id, advertiser_name=advertiser_name,advertiser_email=advertiser_email, create_date=create_date, password_hash=password_hash, active=True)
+
+                    except:
+                        error = "User already exists."
+                        return redirect(url_for('registration'), title=title, active=True, error=error)
+            
+        else:
+            error = "Warning: Passwords didn't match."
+            return redirect(url_for('registration'), title=title, active=True, error=error)
+    else:
+        error = "Warning: Email must be valid email address."
+        return redirect(url_for('registration'), title=title, active=True, error=error)
+    return render_template('main.html', title=title, active=True)
+
+@app.route('/login', methods = ['POST','GET'])
+def login():
+    # Advertiser Login Page
+    title = "Advertiser Login"
+
+    
+
+    if request.method == "POST":
+        try:
+            advertiser_email = request.form.get('email')
+            password = request.form.get('psw')
+            password            = password.encode()
+            salt                = os.environ['SALT_PASS'].encode()
+            password_hash       = hashlib.pbkdf2_hmac("sha256", password, salt, 100000)
+            advertiser_email    = hashlib.pbkdf2_hmac("sha256", advertiser_email, salt, 100000)
+            connection = psycopg2.connect(host          = os.environ['DB_HOST'],
+                                            port        = os.environ['DB_PORT'],
+                                            database    = os.environ['REG_DB_NAME'],
+                                            user        = os.environ['DB_USER'],
+                                            password    = os.environ['DB_PASS'])
+
+            #query = f""" INSERT INTO {os.environ['DB_SCHEMA']}.{os.environ['REG_TABLE']} ({os.environ['REG_COLS']}) VALUES ('{advertiser_id}','{advertiser_name}','{advertiser_email}','{create_date}', '{password_hash}') """
+            query = f""" SELECT * FROM {os.environ['DB_SCHEMA']}.{os.environ['REG_TABLE']} WHERE advertiser_email LIKE {advertiser_email} AND WHERE advertiser_password LIKE {password_hash} """
+            advertiser_info = list(cursor.fetchone())
+
+            cursor = connection.cursor()
+            cursor.execute(query)
+            connection.commit()
+            return render_template('login.html', active=True, title=title) #)advertiser_id=advertiser_id, advertiser_name=advertiser_name,advertiser_email=advertiser_email, create_date=create_date, password_hash=password_hash, active=True)
+
+        except:
+            #error = "Cannot contain a space."
+            return redirect(url_for('registration'), title=title, active=True)
+
+
+    return render_template('login.html', title=title, active=True)
+
+# @app.route('/login/successul', methods = ['GET'])
+# def redeem():
+#     # Advertiser Login Successful Page
+#     title = "Successful login"
+#     return redirect(url_for('main'), title=title, active=True)
+
+@app.route('/redeem', methods = ['GET'])
+def redeem():
+    # Point redemption page, enter Customer UUID and select point redemption reward.
+    title = "Customer Redemption"
+    return render_template('main.html', title=title, active=True)
+
+@app.route('/terms', methods = ['GET'])
+def terms():
+    # Point redemption page, enter Customer UUID and select point redemption reward.
+    title = "Huokan Rewards Terms"
+    return render_template('terms.html', title=title, active=True)
 
 if __name__ == '__main__':
     app.run()
+
+"""
+login_password = hashlib.pbkdf2_hmac(
+    'sha256',
+    password_to_check.encode('utf-8'), # Convert the password to bytes
+    salt, 
+    100000
+)
+
+"""
